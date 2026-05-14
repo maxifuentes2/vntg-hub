6fa53dfff2ea324b2936ce7c12117b1c2d72d608
maxifuentes2 <maximofuentes022@gmail.com>
2026-05-14 19:27:46 -0300

    agregado de correos de admin

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,7 @@
 export const ADMIN_EMAILS = [
-    'maximofuentes022@gmail.com',
-    // Agrega más correos aquí separados por comas, ejemplo:
-    // 'otroadmin@gmail.com',
-    // 'terceradmin@gmail.com'
+    "maximofuentes022@gmail.com",
+    "bautidellu06@gmail.com",
+    "gaspi.tb@gmail.com",
+    "brunogusk2503@gmail.com",
+    "szufia76@gmail.com",
 ];
--- server/config.js
+++ server/config.js
@@ -1,6 +1,9 @@
 module.exports = {
     ADMIN_EMAILS: [
-        'maximofuentes022@gmail.com',
-        // 'otroadmin@gmail.com'
-    ]
+        "maximofuentes022@gmail.com",
+        "bautidellu06@gmail.com",
+        "gaspi.tb@gmail.com",
+        "brunogusk2503@gmail.com",
+        "szufia76@gmail.com",
+    ],
 };
