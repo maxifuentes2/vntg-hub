069c74b092c7c456abe947ac03269dbb1f5cd180
Máximo Fuentes <maximofuentes022@gmail.com>
2026-05-20 22:04:50 -0300

    fix: leer GOOGLE_CLIENT_ID de variable de entorno

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,8 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-// Tu ID de cliente
-const GOOGLE_CLIENT_ID = "696586117644-ded6fdn6sfefsoutitrouo0k93rjgc15.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
