48d9206ea92a71b67c8dc909eea0fbbb5730dee3
Máximo Fuentes <maximofuentes022@gmail.com>
2026-06-09 20:46:42 -0300

    fix: Navbar relee localStorage al cambiar ruta, evita doble carga

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -81,7 +81,7 @@ export default function Navbar() {
                 localStorage.removeItem('vntg_user');
             }
         }
-    }, []);
+    }, [location.pathname]);
 
     useEffect(() => {
         const handleClickOutside = (event) => {
